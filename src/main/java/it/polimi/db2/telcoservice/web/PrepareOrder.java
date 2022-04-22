35e34c315d5224716761898323da5673d2472f5a
andrealesani <80156743+andrealesani@users.noreply.github.com>
2022-04-22 12:18:33 +0200

    Fixed a bug in optional product selection in buy service page

--- src/main/java/it/polimi/db2/telcoservice/web/PrepareOrder.java
+++ src/main/java/it/polimi/db2/telcoservice/web/PrepareOrder.java
@@ -23,24 +23,23 @@ import java.util.Set;
 public class PrepareOrder extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         int serv_pckg_id = Integer.parseInt(request.getParameter("serv_pckg_id"));
         int val_per_id = Integer.parseInt(request.getParameter("val_per_id"));
         List<Integer> opt_prod_ids = new ArrayList<>();
-        int i = 0;
-        while (true) {
-            try {
-                opt_prod_ids.add(Integer.parseInt(request.getParameter("opt_prod_id" + i)));
-                i++;
-            } catch (NumberFormatException e) {
-                break;
-            }
-        }
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         ServicePackage servicePackage = entityManager.find(ServicePackage.class, serv_pckg_id);
+
+        // for each possible optional product included in the package check if it has been checked and sent to the servlet
+        for (int i = 0; i < servicePackage.getOptionalProducts().size(); i++) {
+            try {
+                opt_prod_ids.add(Integer.parseInt(request.getParameter("opt_prod_id" + i)));
+            } catch (NumberFormatException ignored) {}
+        }
+
         ValidityPeriod validityPeriod = entityManager.find(ValidityPeriod.class, val_per_id);
         Set<OptionalProduct> optionalProducts = new HashSet<>();
         for (int id : opt_prod_ids) {
